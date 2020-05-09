fb6f0005514540131c8638a536ca8809d3bed27c
Alexander Sierra <alexandercsierra@gmail.com>
2020-05-09 12:43:05 -0400

    initial commit

---
+++ game.py
@@ -0,0 +1,29 @@
+#player
+    #color
+    #inventory (possibly containing 4 weapons and 4 keys)
+    #droid/no droid
+    #code
+
+#room
+    #code
+    #code of each player
+    #location on board?
+    #is booby trapped?
+        #correct code
+    #inventory
+    #color (key it needs for entrance)
+
+#weapon
+    #name
+    #description
+
+#droid
+    #color
+    #alive/dead
+
+#omega_virus
+    #correct code to win
+    #counter for number of turns
+    #list of phrases
+    
+
